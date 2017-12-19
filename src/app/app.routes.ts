969f444e05021474e64dd997b104376a22350682
Sabah Rod <sabahrod@gmail.com>
2017-12-19 15:21:59 -0800

    pagination

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Route } from '@angular/router';
 import { HomeComponent } from './landing/home/home.component';
 
 export const appRoutes: Route[] = [
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   {path: '', component: HomeComponent, data: {state: ''}},
   {path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule', data: {state: 'dashboard'}}
 ];
--- src/app/dashboard/current-cases/table/table.component.ts
+++ src/app/dashboard/current-cases/table/table.component.ts
@@ -12,17 +12,55 @@ import { CaseBackendService } from '../../../backend/case-backend';
 export class TableComponent implements OnInit {
 
   public cases: Case[] = [];
+  public paginatedCases: Case[][] = [];
+  public selectedIndex: number = 0;
 
   constructor(private currentCasesService: CurrentCasesService, private caseBackend: CaseBackendService) {
   }
 
   public ngOnInit(): void {
     this.caseBackend.datasource.connect().subscribe((cases: Case[]) => {
-      this.cases = cases;
+      this.paginatedCases = this.paginate(cases);
+      if (this.paginatedCases.length > 0) {
+        this.cases = this.paginatedCases[this.selectedIndex];
+      }
     });
     this.currentCasesService.filters().subscribe((filters: InputCategory[]) => {
       this.filterCases(filters);
     });
+  }
+
+  public selectIndex(i: number): void {
+    this.selectedIndex = i;
+    if (this.paginatedCases.length > 0 && this.paginatedCases[this.selectedIndex]) {
+      this.cases = this.paginatedCases[this.selectedIndex];
+    }
+  }
+
+  public previous(): void {
+    if (this.selectedIndex >= 0 && this.paginatedCases[this.selectedIndex - 1]){
+      this.selectedIndex = this.selectedIndex - 1;
+      this.cases = this.paginatedCases[this.selectedIndex];
+    }
+  }
+
+  public next(): void {
+    if (this.selectedIndex >= 0 && this.paginatedCases[this.selectedIndex + 1]){
+      this.selectedIndex = this.selectedIndex + 1;
+      this.cases = this.paginatedCases[this.selectedIndex];
+    }
+  }
+
+  public isActive(i: number): boolean {
+    return i === this.selectedIndex;
+  }
+
+  private paginate(cases: Case[]): Case[][] {
+    const results: Case[][] = [];
+    while (cases.length > 0) {
+      results.push(cases.splice(0, 10));
+    }
+    return results;
   }
 
   private filterCases(filters: InputCategory[]): void {
